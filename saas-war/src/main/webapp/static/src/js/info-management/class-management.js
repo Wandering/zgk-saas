8d2a008d8b6d149990724b941cca7d49cd404c12
machengcheng <machengcheng@thinkjoy.cn>
2016-11-18 01:10:44 +0800

    machengcheng

--- saas-war/src/main/webapp/static/src/js/info-management/class-management.js
+++ saas-war/src/main/webapp/static/src/js/info-management/class-management.js
@@ -328,7 +328,7 @@ UploadData.prototype = {
         uploadDataHtml.push('<span id="fileList" class="uploader-list"></span>');
         uploadDataHtml.push('<button class="btn btn-info btn-import" id="btn-import">导入班级数据Excel</button>');
         uploadDataHtml.push('</span>');
-        uploadDataHtml.push('<a href="javascript: void(0);" class="download-link">请先导出Excel模板，进行填写</a>');
+        uploadDataHtml.push('<a href="javascript: void(0);" id="downloadBtn" class="download-link">请先导出Excel模板，进行填写</a>');
         uploadDataHtml.push('<button class="btn btn-cancel cancel-btn" id="cancel-download-btn">取消</button>');
         uploadDataHtml.push('</div>');
         layer.open({
@@ -499,5 +499,112 @@ $(document).on('click', '#class-settings-btn', function () {
             var classManagement = new ClassManagement();
             classManagement.init();
         }
+    });
+});
+
+
+jQuery(function () {
+    var $ = jQuery,
+        $list = $('#fileList'),
+    // 优化retina, 在retina下这个值是2
+        ratio = window.devicePixelRatio || 1,
+
+    // 缩略图大小
+        thumbnailWidth = 100 * ratio,
+        thumbnailHeight = 100 * ratio,
+
+    // Web Uploader实例
+        uploader;
+
+    // 初始化Web Uploader
+    uploader = WebUploader.create({
+
+        // 自动上传。
+        auto: true,
+
+        // swf文件路径
+        swf: BASE_URL + '/js/Uploader.swf',
+
+        // 文件接收服务端。
+        server: rootPath + '/config/upload/class/' + tnId + '.do',
+
+        // 选择文件的按钮。可选。
+        // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+        pick: '#btn-import',
+
+        // 只允许选择文件，可选。
+        accept: {
+            title: 'excel',
+            extensions: 'xls',
+            mimeTypes: 'application/vnd.ms-excel'
+        },
+        fileVal: 'inputFile',
+        duplicate: new Date()
+
+    });
+
+    // 当有文件添加进来的时候
+    uploader.on('fileQueued', function (file) {
+        var $li = $(
+            '<div id="' + file.id + '" class="file-item thumbnail">' +
+                //'<img>' +
+            '<div class="info">' + file.name + '</div>' +
+            '</div>'
+        )
+        //    $img = $li.find('img');
+        //
+        $list.append($li);
+
+        // 创建缩略图
+        //uploader.makeThumb( file, function( error, src ) {
+        //    if ( error ) {
+        //        $img.replaceWith('<span>不能预览</span>');
+        //        return;
+        //    }
+        //
+        //    $img.attr( 'src', src );
+        //}, thumbnailWidth, thumbnailHeight );
+    });
+
+    // 文件上传过程中创建进度条实时显示。
+    uploader.on('uploadProgress', function (file, percentage) {
+        var $li = $('#' + file.id),
+            $percent = $li.find('.progress span');
+
+        // 避免重复创建
+        if (!$percent.length) {
+            $percent = $('<p class="progress"><span></span></p>')
+                .appendTo($li)
+                .find('span');
+        }
+
+        $percent.css('width', percentage * 100 + '%');
+    });
+
+    // 文件上传成功，给item添加成功class, 用样式标记上传成功。
+    uploader.on('uploadSuccess', function (file) {
+        $('#add-btn,#del-batch-btn').addClass('disabled');
+        $('.del-btn').removeClass('del-btn').addClass('disabled');
+        $('#' + file.id).addClass('upload-state-done');
+        layer.msg('导入成功,请进行下一流程!');
+
+    });
+
+    // 文件上传失败，现实上传出错。
+    uploader.on('uploadError', function (file) {
+        var $li = $('#' + file.id),
+            $error = $li.find('div.error');
+
+        // 避免重复创建
+        if (!$error.length) {
+            $error = $('<div class="error"></div>').appendTo($li);
+        }
+
+        $error.text('上传失败');
+    });
+
+    // 完成上传完了，成功或者失败，先删除进度条。
+    uploader.on('uploadComplete', function (file) {
+        $('#' + file.id).find('.progress').remove();
     });
 });
