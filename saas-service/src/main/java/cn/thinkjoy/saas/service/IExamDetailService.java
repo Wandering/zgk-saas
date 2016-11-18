84afc57b557afc1e501717b23f92bb241d1b3ff2
刘晓文 <liuxiaowen@thinkjoy.cn>
2016-11-18 18:07:49 +0800

    fk

--- saas-service/src/main/java/cn/thinkjoy/saas/service/IExamDetailService.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/IExamDetailService.java
@@ -41,4 +41,10 @@ public interface IExamDetailService<D extends IBaseDAO<T>, T extends BaseDomain>
     List<Map<String, Object>> getAvgScoresForClassStudent(Map<String, String> paramMap);
 
     List<Map<String, Object>> getMostAttentionListForClass(Map<String, String> paramMap);
+
+    boolean checkIsTableExist(Map<String, String> paramMap);
+
+    boolean checkIsColumnExist(Map<String, String> paramMap);
+
+    List<Map<String, Object>> getClassBossList(Map<String, String> paramMap);
 }
