dee9fc559cdd19628c2dbc1d0405ca20750db7fa
hzuo <hzuo@thinkjoy.cn>
2016-11-21 17:45:25 +0800

    选课分析

--- saas-service/src/main/java/cn/thinkjoy/saas/service/impl/bussiness/reform/SelectClassesGuideServiceImpl.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/impl/bussiness/reform/SelectClassesGuideServiceImpl.java
@@ -119,4 +119,84 @@ public class SelectClassesGuideServiceImpl implements ISelectClassesGuideService
         return groups;
     }
 
+    @Override
+    public Map<Integer,Map<String, Integer>> selectNumberByYear(int tnId){
+        Map<String, Object> map=new HashMap<>();
+        map.put("tnId",tnId);
+        map.put("domain","student");
+        map.put("enName","student_major_type");
+        if(selectClassesGuideDAO.selectEnName(map)<1){
+            ExceptionUtil.throwException(ErrorCode.TABLE_NOT_EXIST);
+        }
+        String tableName = "saas_"+tnId+"_student_excel";
+        List<Map<String,String>> mapList = Lists.newArrayList();
+        map.put("tableName",tableName);
+        mapList = selectClassesGuideDAO.selectStudentExcel(map);
+//        Map<String, Integer> map2=new HashMap<>();
+        Map<Integer, Map<String, Integer>> yearMap=new HashMap<>();
+        for (Map<String,String> map1:mapList){
+            int year=Integer.valueOf(String.valueOf(map1.get("student_class_in_year")))+3;
+            if(!yearMap.containsKey(year)){
+                yearMap.put(year,new HashMap<String, Integer>());
+            }
+            String[] types=map1.get("student_major_type").split("-");
+            for(String type:types){
+                if(!yearMap.get(year).containsKey(type)){
+                    yearMap.get(year).put(type, 0);
+                }
+                yearMap.get(year).put(type, yearMap.get(year).get(type)+1);
+            }
+
+        }
+        return yearMap;
+    }
+
+    @Override
+    public Map<String,Object> selectTypeAnalysis(int tnId,String studentGrade){
+        Map<String, Object> map=new HashMap<>();
+        String tableName = "saas_"+tnId+"_student_excel";
+        map.put("tableName",tableName);
+        map.put("student_grade",studentGrade);
+        List<Map<String,String>> mapList = Lists.newArrayList();
+        mapList = selectClassesGuideDAO.selectStudentExcel(map);
+        Map<String, Integer> typeMap=new HashMap<>();
+        for(Map<String,String> map1:mapList){
+            String[] types=map1.get("student_major_type").split("-");
+            for(String type:types){
+                if(!typeMap.containsKey(type)){
+                    typeMap.put(type, 0);
+                }
+                typeMap.put(type, typeMap.get(type) + 1);
+            }
+        }
+
+
+        //从saas_enrolling_ratio中获取上线率
+        String t=selectClassesGuideDAO.getEnrollingPercent();
+        map.put("tnId",tnId);
+        map.put("grade",studentGrade);
+        map.put("examId",selectClassesGuideDAO.selectExamId(map));
+        int count=selectClassesGuideDAO.selectStudentNumber(map);
+        Long top=Math.round(count * Double.valueOf(t));
+        map.put("top",top.intValue());
+        List<String> selectCoursesList=selectClassesGuideDAO.selectLimitStudent(map);
+        Map<String, Integer> limitTypeMap=new HashMap<>();
+        for(String selectCourses:selectCoursesList){
+            String[] types=selectCourses.split("@");
+            for(String type:types){
+                if(!limitTypeMap.containsKey(type)){
+                    limitTypeMap.put(type, 0);
+                }
+                limitTypeMap.put(type, limitTypeMap.get(type) + 1);
+            }
+        }
+        Map<String,Object> returnMap=new HashMap<>();
+        returnMap.put("oneTypeMap",typeMap);
+        returnMap.put("count",count);
+        returnMap.put("top",top.intValue());
+        returnMap.put("limitTypeMap",limitTypeMap);
+
+        return returnMap;
+    }
+
 }
