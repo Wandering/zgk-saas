801cf183ed33505c57714115bea71e055c184a2c
douzi <douzy75952@hotmail.com>
2016-11-23 16:05:47 +0800

    gradle refresh

--- saas-service/src/main/java/cn/thinkjoy/saas/service/common/EnumUtil.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/common/EnumUtil.java
@@ -20,7 +20,7 @@ public class EnumUtil {
     public static final String TEACHER_EDUCATION_GRADE="所教年级";
     public static final String TEACHER_EDUCATION_CLASS="所教班级";
     public static final String TEACHER_EDUCATION_MAJOYTYPE="所教科目";
-    public static final String[] TEACHER_EDUCATION_MAJOYTYPE_ARR={"物理","化学","生物","政治","历史","地理","通用技术"};
+    public static final String[] TEACHER_EDUCATION_MAJOYTYPE_ARR={"语文","数学","物理","化学","生物","政治","历史","地理","通用技术"};
 
     /**
      * 错误编码   规则: 【业务块描述-错误描述】
