5379a38697a19209577033605e80681dc09d2383
杨永平 <ypyang@qtonecloud.cn>
2016-12-07 10:54:50 +0800

    重新生成表结构,部分接口提交

--- saas-war/src/main/java/cn/thinkjoy/saas/controller/bussiness/ScheduleTaskController.java
+++ saas-war/src/main/java/cn/thinkjoy/saas/controller/bussiness/ScheduleTaskController.java
@@ -1,13 +1,13 @@
 package cn.thinkjoy.saas.controller.bussiness;
 
 import cn.thinkjoy.common.exception.BizException;
-import cn.thinkjoy.common.utils.SqlOrderEnum;
 import cn.thinkjoy.saas.common.UserContext;
 import cn.thinkjoy.saas.core.Constant;
 import cn.thinkjoy.saas.domain.JwScheduleTask;
 import cn.thinkjoy.saas.dto.JwScheduleTaskDto;
 import cn.thinkjoy.saas.enums.ErrorCode;
 import cn.thinkjoy.saas.enums.GradeEnum;
+import cn.thinkjoy.saas.enums.StatusEnum;
 import cn.thinkjoy.saas.enums.TermEnum;
 import cn.thinkjoy.saas.service.IJwScheduleTaskService;
 import cn.thinkjoy.saas.service.common.ExceptionUtil;
@@ -61,6 +61,64 @@ public class ScheduleTaskController {
         return jwScheduleTaskService.add(jwScheduleTask)>0;
     }
 
+    /**
+     * 修改排课任务
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/updateScheduleTask")
+    public boolean updateScheduleTask(@RequestParam Integer id,
+                                    @RequestParam String scheduleName,
+                                    @RequestParam String grade,
+                                    @RequestParam String year,
+                                    @RequestParam String term){
+        //参数校验
+        if (StringUtils.isEmpty(scheduleName)||StringUtils.isEmpty(grade)||StringUtils.isEmpty(year)||StringUtils.isEmpty(term)){
+            ExceptionUtil.throwException(ErrorCode.PARAN_NULL);
+        }
+        JwScheduleTask jwScheduleTask = new JwScheduleTask();
+        jwScheduleTask.setId(id);
+        jwScheduleTask.setScheduleName(scheduleName);
+        jwScheduleTask.setGrade(grade);
+        jwScheduleTask.setTerm(term);
+        jwScheduleTask.setYear(year);
+        Integer tnId = Integer.valueOf(UserContext.getCurrentUser().getTnId());
+        jwScheduleTask.setTnId(tnId);
+        jwScheduleTask.setCreateDate(System.currentTimeMillis());
+        jwScheduleTask.setStatus(Constant.SCHEDULE_TASK_INIT_STATUS);
+        return jwScheduleTaskService.update(jwScheduleTask)>0;
+    }
+
+    /**
+     * 删除排课任务
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/deleteScheduleTask")
+    public boolean deleteScheduleTask(@RequestParam Integer id){
+        //参数校验
+        JwScheduleTask jwScheduleTask = new JwScheduleTask();
+        jwScheduleTask.setId(id);
+        jwScheduleTask.setDelStatus(id);
+        return jwScheduleTaskService.update(jwScheduleTask)>0;
+    }
+
+    /**
+     * 查询单个排课任务
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/fetchScheduleTask")
+    public JwScheduleTask fetchScheduleTask(@RequestParam Integer id){
+        //参数校验
+         new JwScheduleTask();
+        Map<String,Object> map = Maps.newHashMap();
+        map.put("id",id);
+        map.put("delStatus",StatusEnum.Y.getCode());
+        JwScheduleTask jwScheduleTask = (JwScheduleTask) jwScheduleTaskService.queryOne(map);
+        return jwScheduleTask==null?new JwScheduleTask():jwScheduleTask;
+    }
+
     @ResponseBody
     @RequestMapping("/queryGradeInfo")
     public List<Map<String,Object>> queryGradeInfo(){
@@ -86,6 +144,7 @@ public class ScheduleTaskController {
         List<JwScheduleTaskDto> returnList = new ArrayList<>();
         Map<String,Object> map = Maps.newHashMap();
         map.put("tnId",UserContext.getCurrentUser().getTnId());
+        map.put("delStatus", StatusEnum.Y.getCode());
         List<JwScheduleTask> dataList = jwScheduleTaskService.queryList(map,"createDate","desc");
         if (dataList!=null&&dataList.size()>0){
             returnList = JwScheduleTask2Dto(dataList);
@@ -96,6 +155,7 @@ public class ScheduleTaskController {
     @ResponseBody
     @RequestMapping("/checkTaskBaseInfo")
     public boolean checkTaskBaseInfo(@RequestParam Integer taskId){
+        //检查表字段是否完整
         if (false){
             throw  new BizException(ErrorCode.TASK_ERROR.getCode(),"您还未填写****、****、*****字段信息，请至学生管理中完善");
         }
