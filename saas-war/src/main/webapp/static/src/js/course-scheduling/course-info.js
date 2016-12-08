8416ae5e37e34ac1cdaf88812f9c9793eb7207fa
dengshaofei <dengsf007@gmail.com>
2016-12-08 09:52:43 +0800

    commit

--- saas-war/src/main/webapp/static/src/js/course-scheduling/course-info.js
+++ saas-war/src/main/webapp/static/src/js/course-scheduling/course-info.js
@@ -44,34 +44,36 @@ CourseInfo.prototype = {
                 'taskId': taskId
             },
             function (res) {
-                //var res = {
-                //    "bizData": [
-                //        {
-                //            "courseId": "课程ID",
-                //            "courseName": "课程名",
-                //            "time": "课时"
-                //        },
-                //        {
-                //            "courseId": "课程ID",
-                //            "courseName": "课程名",
-                //            "time": "课时"
-                //        },
-                //        {
-                //            "courseId": "课程ID",
-                //            "courseName": "课程名",
-                //            "time": "课时"
-                //        }
-                //    ],
-                //    "rtnCode": "0000000",
-                //    "ts": 1480990693884
-                //};
+                var res = {
+                    "bizData": [
+                        {
+                            "courseId": "课程ID",
+                            "courseName": "课程名",
+                            "time": "课时"
+                        },
+                        {
+                            "courseId": "课程ID",
+                            "courseName": "课程名",
+                            "time": "课时"
+                        },
+                        {
+                            "courseId": "课程ID",
+                            "courseName": "课程名",
+                            "time": "课时"
+                        }
+                    ],
+                    "rtnCode": "0000000",
+                    "ts": 1480990693884
+                };
                 if (res.rtnCode == "0000000") {
-                    $('#teacher-list').html('');
-                    var myTemplate = Handlebars.compile($("#teacher-template").html());
-                    Handlebars.registerHelper("addOne", function (index, options) {
-                        return parseInt(index) + 1;
-                    });
-                    $('#teacher-list').html(myTemplate(res));
+                    if(res.bizData.length>0){
+                        $('#teacher-list').html('');
+                        var myTemplate = Handlebars.compile($("#teacher-template").html());
+                        Handlebars.registerHelper("addOne", function (index, options) {
+                            return parseInt(index) + 1;
+                        });
+                        $('#teacher-list').html(myTemplate(res));
+                    }
                 }
             }, function (res) {
                 layer.msg(res.msg);
