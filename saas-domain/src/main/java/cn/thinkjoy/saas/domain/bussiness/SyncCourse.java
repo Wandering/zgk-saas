9800f2839ed948a7785a90933227784ae55480f2
douzi <douzy75952@hotmail.com>
2016-12-08 15:23:05 +0800

    add class room

---
+++ saas-domain/src/main/java/cn/thinkjoy/saas/domain/bussiness/SyncCourse.java
@@ -0,0 +1,34 @@
+package cn.thinkjoy.saas.domain.bussiness;
+
+/**
+ * Created by douzy on 16/12/8.
+ */
+public class SyncCourse {
+    private String major;
+    private String stuClass;
+    private String grade;
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getStuClass() {
+        return stuClass;
+    }
+
+    public void setStuClass(String stuClass) {
+        this.stuClass = stuClass;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
