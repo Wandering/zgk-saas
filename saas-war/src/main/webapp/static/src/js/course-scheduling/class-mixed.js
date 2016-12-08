336c93efbd2ed2c4d15bf8df818b532cd9c682c0
pdeng <pdeng@thinkjoy.cn>
2016-12-08 15:12:11 +0800

    Merge branch 'feature/schedule' of http://git.qtonecloud.cn/scm/gk360/zgk-saas into feature/schedule
    # Conflicts:
    #	saas-war/src/main/resources/config/environment.properties

--- saas-war/src/main/webapp/static/src/js/course-scheduling/class-mixed.js
+++ saas-war/src/main/webapp/static/src/js/course-scheduling/class-mixed.js
@@ -1,8 +1,99 @@
 /**
- * Created by machengcheng on 16/12/6.
+ * @wiki: http://wiki.qtonecloud.cn/pages/viewpage.action?pageId=44458344
+ * 全局常量
+ * @type {{tnId: *, typ: string}}
  */
-$(function () {
+var GLOBAL_CONSTANT = {
+    tnId: Common.cookie.getCookie('tnId'), //租户ID
+    taskId: Common.cookie.getCookie('taskId'), //租户ID
+}
+alert(GLOBAL_CONSTANT.taskId)
 
-
-
-});
+/**
+ * 排课任务 -> 排课规则设置 ->合班
+ * @type {{init: MixedClass.init, fetchAndRenderSubject: MixedClass.fetchAndRenderSubject, fetchBelongClass: MixedClass.fetchBelongClass}}
+ */
+var MixedClass = {
+    init: function () {
+        this.fetchAndRenderSubject();
+        this.fetchBelongClass();
+        console.info(Common.cookie.getCookie('taskId'));
+    },
+    fetchAndRenderSubject: function () {
+        Common.ajaxFun('/scheduleTask/queryCourseInfoByTaskId.do', 'get', {
+            'taskId': '1'
+        }, function (res) {
+            var res = {
+                "bizData": [{
+                    "courseId": "课程ID",
+                    "courseName": "语文",
+                    "time": "课时"
+                }, {
+                    "courseId": "课程ID",
+                    "courseName": "通用技术",
+                    "time": "课时"
+                }, {
+                    "courseId": "课程ID",
+                    "courseName": "数学",
+                    "time": "课时"
+                }],
+                "rtnCode": "0000000",
+                "ts": 1480990693884
+            }
+            if (res.rtnCode == '0000000') {
+                var tpl = Handlebars.compile($('#course-select-tpl').html());
+                $('#course-select').html(tpl(res.bizData));
+            } else {
+                layer.msg(res.msg);
+            }
+        });
+    },
+    fetchBelongClass: function () {
+        Common.ajaxFun('/manage/class/' + GLOBAL_CONSTANT.tnId + '/getTenantCustomData.do', 'GET', {}, function (res) {
+            if (res.rtnCode == "0000000") {
+                var tpl = Handlebars.compile($('#choose-class-list-tpl').html());
+                $('#choose-class-list').html(tpl(res.bizData));
+            }
+        });
+    },
+    addEvent: function () {
+        var that = this;
+        $('.mixed-class-btn').click(function () {
+            that.submitMixedClass();
+        });
+    },
+    submitMixedClass: function () {
+        var that = this;
+        Common.ajaxFun('/mergeClassController/addMergeInfo.do', 'get', {
+            'tnId': GLOBAL_CONSTANT.tnId,
+            'taskId': GLOBAL_CONSTANT.taskId,
+            'courseId': '',
+            'classIds': '',//班级Id，多个班级有英文逗号分隔
+        }, function (res) {
+            if (res.rtnCode === '0000000') {
+                console.info(res);
+                that.fetchResult();
+            } else {
+                layer.msg(res.msg);
+            }
+        });
+    },
+    fetchResult: function () {
+        var that = this;
+        Common.ajaxFun('/mergeClassController/getMergeInfo.do', 'get', {
+            'tnId': GLOBAL_CONSTANT.tnId,
+            'taskId': GLOBAL_CONSTANT.taskId,
+            'grade': '',
+        }, function (res) {
+            if (res.rtnCode === '0000000') {
+                that.renderResult(res.bizData)
+            } else {
+                layer.msg(res.msg);
+            }
+        });
+    },
+    renderResult:function(dataJson){
+        console.info('dataJson',dataJson);
+    }
+}
+MixedClass.init();
