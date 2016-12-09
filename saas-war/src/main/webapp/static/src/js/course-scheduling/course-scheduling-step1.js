dcce684eefb0d14bb1e317dc91f661e04e224640
dengshaofei <dengsf007@gmail.com>
2016-12-09 10:30:39 +0800

    commit

--- saas-war/src/main/webapp/static/src/js/course-scheduling/course-scheduling-step1.js
+++ saas-war/src/main/webapp/static/src/js/course-scheduling/course-scheduling-step1.js
@@ -1,7 +1,6 @@
-
 var taskId = Common.cookie.getCookie('taskId');
 
-function TeachDate(){
+function TeachDate() {
     this.init();
     this.state = false;
 }
@@ -12,36 +11,27 @@ TeachDate.prototype = {
 
     },
     // 查询学习时间
-    queryTeachTime:function(taskId){
+    queryTeachTime: function (taskId) {
         var that = this;
         Common.ajaxFun('/teachTime/queryTeachTime.do', 'GET', {
-            'taskId':taskId
-        },
+                'taskId': taskId
+            },
             function (res) {
                 console.log(res.bizData)
-
-
                 if (res.rtnCode == "0000000") {
                     var teachDate = res.bizData.teachDate;
                     var teachTime = res.bizData.teachTime;
 
-                    //if(!res.bizData.teachDate && !res.bizData.teachTime){
-                    //    alert(3)
-                    //    that.state
-                    //}else{
-                    //
-                    //}
-
-
-                    if(teachDate){
-                        for(var i in teachDate.split('|')){
-                            $('.week-list input[data="'+ teachDate.split('|')[i]  +'"]').attr('checked','checked');
+                    if (!teachDate && !teachTime) {
+                        that.state = false;
+                    } else {
+                        for (var i in teachDate.split('|')) {
+                            $('.week-list input[data="' + teachDate.split('|')[i] + '"]').attr('checked', 'checked');
                         }
-                    }
-                    if(teachTime){
-                        $('#morning-list option[value="'+ teachTime.split("")[0] +'"]').attr('selected','selected');
-                        $('#afternoon-list option[value="'+ teachTime.split("")[1] +'"]').attr('selected','selected');
-                        $('#evening-list option[value="'+ teachTime.split("")[2] +'"]').attr('selected','selected');
+                        $('#morning-list option[value="' + teachTime.split("")[0] + '"]').attr('selected', 'selected');
+                        $('#afternoon-list option[value="' + teachTime.split("")[1] + '"]').attr('selected', 'selected');
+                        $('#evening-list option[value="' + teachTime.split("")[2] + '"]').attr('selected', 'selected');
+                        that.state = true;
                     }
                 }
 
@@ -50,44 +40,78 @@ TeachDate.prototype = {
             });
     },
     // 保存教学时间
-    saveTeachTime:function(taskId,teachDate,teachTime){
+    saveTeachTime: function (taskId, teachDate, teachTime) {
         Common.ajaxFun('/teachTime/saveTeachTime.do', 'POST', {
-            'taskId':taskId,
-            'teachDate':teachDate,
-            'teachTime':teachTime
-        },
-        function (res) {
-            if (res.rtnCode == "0000000" && res.bizData==true) {
-                layer.msg('保存成功!');
-            }
-        }, function (res) {
-            layer.msg(res.msg);
-        });
+                'taskId': taskId,
+                'teachDate': teachDate,
+                'teachTime': teachTime
+            },
+            function (res) {
+                if (res.rtnCode == "0000000" && res.bizData == true) {
+                    layer.msg('保存成功!');
+                }
+            }, function (res) {
+                layer.msg(res.msg);
+            });
     }
 };
 
 var TeachDateIns = new TeachDate();
 
-$(function(){
+$(function () {
     TeachDateIns.queryTeachTime(taskId);
-    // 保存
-    $('#btn-save-base').on('click',function(){
+
+    // 保存确认
+    $('#btn-save-base').on('click', function () {
+        var that = $(this);
         var checkboxLen = $('.week-list input:checked').length;
-        var weekV= $('.week-list input:checked');
+        var weekV = $('.week-list input:checked');
         if (checkboxLen == 0) {
-            layer.tips('每周上课天数至少选择一项', $(this));
+            layer.tips('每周上课天数至少选择一项', that);
             return false;
         }
         var teachDate = [];
-        $.each(weekV,function(i,v){
-            teachDate.push($(this).attr('data'));
+        $.each(weekV, function (i, v) {
+            teachDate.push(that.attr('data'));
         });
         var morningNum = $('#morning-list').val();
         var afternoonNum = $('#afternoon-list').val();
         var eveningNum = $('#evening-list').val();
-        var teachTime = morningNum+afternoonNum+eveningNum;
-        TeachDateIns.saveTeachTime(taskId,teachDate.join('|'),teachTime);
+        var teachTime = morningNum + afternoonNum + eveningNum;
+        console.log(TeachDateIns.state)
+        if(TeachDateIns.state==true){
+            layer.confirm('如果更改基本信息设置，已设置的排课条件有可能会被清除，请谨慎操作！', {
+                btn: ['确定', '关闭'] //按钮
+            }, function () {
+                TeachDateIns.saveTeachTime(taskId, teachDate.join('|'), teachTime);
+            }, function () {
+
+            });
+        }else{
+            TeachDateIns.saveTeachTime(taskId, teachDate.join('|'), teachTime);
+        }
     });
+
+
+    // 保存
+
+    //$('#btn-save-base').on('click',function(){
+    //    var checkboxLen = $('.week-list input:checked').length;
+    //    var weekV= $('.week-list input:checked');
+    //    if (checkboxLen == 0) {
+    //        layer.tips('每周上课天数至少选择一项', $(this));
+    //        return false;
+    //    }
+    //    var teachDate = [];
+    //    $.each(weekV,function(i,v){
+    //        teachDate.push($(this).attr('data'));
+    //    });
+    //    var morningNum = $('#morning-list').val();
+    //    var afternoonNum = $('#afternoon-list').val();
+    //    var eveningNum = $('#evening-list').val();
+    //    var teachTime = morningNum+afternoonNum+eveningNum;
+    //    TeachDateIns.saveTeachTime(taskId,teachDate.join('|'),teachTime);
+    //});
 
 
 });
