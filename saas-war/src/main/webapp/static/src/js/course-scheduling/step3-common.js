8427cde59704d7ee5a372822196a5c7cb0689588
machengcheng <machengcheng@thinkjoy.cn>
2016-12-15 11:55:09 +0800

    machengcheng

--- saas-war/src/main/webapp/static/src/js/course-scheduling/step3-common.js
+++ saas-war/src/main/webapp/static/src/js/course-scheduling/step3-common.js
@@ -1,12 +1,14 @@
 /**
  * 地址Hash处理
  */
+var taskId = Common.cookie.getCookie('taskId');
 
 var HashHandle = {
     init: function () {
         this.hashArr = ['#class', '#teacher', '#student', '#all'];
         this.addEvent();
         this.hashOperate();
+        this.initStatus();
     },
     addEvent: function () {
         $('#role-scheduling-tab .role-tab li').click(function () {
@@ -29,8 +31,45 @@ var HashHandle = {
                 $('#control-jsp .bottom-page').eq(i).removeClass('dh').siblings().addClass('dh');
             }
         }
+    },
+    initStatus: function () {
+        Common.ajaxFun('/scheduleTask/queryScheduleTaskStatus', 'GET', {
+            'taskId': taskId
+        }, function (res) {
+            if (res.rtnCode == "0000000") {
+                var data = res.bizData;
+                switch (parseInt(data)) {
+                    case 1:
+                            $('.btn-one-key').removeClass('dh');
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        $('.btn-one-key').addClass('dh');
+                        $('.info-modify').removeClass('dh');
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }, function (res) {
+            layer.msg("出错了");
+        }, true);
+    },
+    updateStatus: function () {
+        Common.ajaxFun('/scheduleTask/updateScheduleTaskStatus', 'GET', {
+            'taskId': taskId
+        }, function (res) {
+            if (res.rtnCode == "0000000") {
+                var data = res.bizData;
+                $('#role-scheduling-tab, #step3-child-class').removeClass('dh');
+            }
+        }, function (res) {
+            layer.msg("出错了");
+        }, true);
     }
-}
+};
 HashHandle.init();
 
 
@@ -39,19 +78,23 @@ HashHandle.init();
  * lockSubject 0,1
  */
 $('.btn-one-key').click(function(){
-    $(this).hide();
-    $('#role-scheduling-tab,#step3-child-class').show();
-    Common.cookie.setCookie('lockSubject',1);
-})
-if(Common.cookie.getCookie('lockSubject') == 1){
-    $('.btn-one-key').hide();
-    $('#role-scheduling-tab,#step3-child-class').removeClass('dh');
-}
-
-$(function () {
-
-    var flag = Common.checkInfoIsPerfect(taskId);
-    if (!flag) {
-        window.location.href = '/course-scheduling-step1';
-    }
+    HashHandle.updateStatus();
+    //$(this).hide();
+    //$('#role-scheduling-tab,#step3-child-class').show();
+    //Common.cookie.setCookie('lockSubject',1);
 });
+//if(Common.cookie.getCookie('lockSubject') == 1){
+//    $('.btn-one-key').hide();
+//    $('#role-scheduling-tab, #step3-child-class').removeClass('dh');
+//}
+$(document).on('click', '.retry-scheduling, .look-origin-schedule', function () {
+    $('#role-scheduling-tab, #step3-child-class').removeClass('dh');
+});
+
+//$(function () {
+//
+//    var flag = Common.checkInfoIsPerfect(taskId);
+//    if (!flag) {
+//        window.location.href = '/course-scheduling-step1';
+//    }
+//});
