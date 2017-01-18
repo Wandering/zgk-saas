1bfbff7167f93b0ff197dd9a43cd8a8804ebce6c
杨国荣 <yangguorong@imzhiliao.com>
2017-01-18 16:01:43 +0800

    选课指导提供接口

---
+++ saas-war/src/main/java/cn/thinkjoy/saas/controller/bussiness/CourseGuidanceController.java
@@ -0,0 +1,39 @@
+package cn.thinkjoy.saas.controller.bussiness;
+
+import cn.thinkjoy.common.restful.apigen.annotation.ApiDesc;
+import cn.thinkjoy.gk.api.IMajoredApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Created by yangguorong on 17/1/18.
+ *
+ * 选课指导模块
+ */
+@Controller
+@RequestMapping("/guidance")
+public class CourseGuidanceController {
+
+    @Autowired
+    private IMajoredApi iMajoredApi;
+
+    @ResponseBody
+    @ApiDesc(value = "查询有招生计划的年份")
+    @RequestMapping(value = "getEnrollingYear",method = RequestMethod.GET)
+    public List<String> getEnrollingYear(){
+        return iMajoredApi.getEnrollingYear();
+    }
+
+    @ResponseBody
+    @ApiDesc(value = "根据年份查询个种选课组合的招生院校")
+    @RequestMapping(value = "selectMajorTopCount",method = RequestMethod.GET)
+    public Object selectMajorTopCount(@RequestParam String year){
+        return iMajoredApi.selectMajorTopCount(35,year);
+    }
+}
