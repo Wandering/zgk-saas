66b117074fd5b21571826b3de7f9fcad5fd12ed6
杨国荣 <yangguorong@imzhiliao.com>
2017-02-13 10:49:04 +0800

    修改bug

---
+++ saas-war/src/main/java/cn/thinkjoy/saas/controller/bussiness/StudentController.java
@@ -0,0 +1,15 @@
+package cn.thinkjoy.saas.controller.bussiness;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by yangguorong on 17/2/13.
+ *
+ * 学生管理
+ */
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+}
