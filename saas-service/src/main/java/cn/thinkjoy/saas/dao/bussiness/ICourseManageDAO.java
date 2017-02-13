09d6078836ec2f6b76450d7850f89ed343a42246
douzi <douzy75952@hotmail.com>
2017-02-13 16:55:16 +0800

    Course Manage

---
+++ saas-service/src/main/java/cn/thinkjoy/saas/dao/bussiness/ICourseManageDAO.java
@@ -0,0 +1,10 @@
+package cn.thinkjoy.saas.dao.bussiness;
+
+import cn.thinkjoy.common.dao.IBaseDAO;
+import cn.thinkjoy.saas.domain.bussiness.CourseManage;
+
+/**
+ * Created by douzy on 17/2/13.
+ */
+public interface ICourseManageDAO extends IBaseDAO<CourseManage> {
+}
