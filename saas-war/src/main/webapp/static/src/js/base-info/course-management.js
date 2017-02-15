938f86b22323431f3d5d7d1cc4752fbfa1a759e4
dengshaofei <dengsf007@gmail.com>
2017-02-15 15:35:32 +0800

    222

--- saas-war/src/main/webapp/static/src/js/base-info/course-management.js
+++ saas-war/src/main/webapp/static/src/js/base-info/course-management.js
@@ -12,60 +12,34 @@ CourseManagement.prototype = {
         this.getCourse();
         this.addCourse();
         this.updateCourse();
-        this.delCourse();
         this.getGrade();
     },
     // 语文、数学、英语、物理、化学、生物、政治、历史、地理  默认课程不可编辑\不可修改\不可删除
     // 通用技术 不可编辑\不可修改\不可删除
-    // 查询课程基础信息
-    getDefaultInfo:function(data){
-        //var courseArr = ['语文','数学','英语','物理','化学','生物','地理','历史','生物'];
-        //var courseTemplate = Handlebars.compile($("#course-tbody-template").html());
-        //Handlebars.registerHelper("addOne", function (index, options) {
-        //    return parseInt(index) + 1;
-        //});
-        //$('#course-tbody').html(courseTemplate(courseArr));
-
-        console.log(data);
-
-        var dataObj = data.bizData.courses;
-        var courseHtml = '';
-        for(var i=0;i<dataObj.length;i++){
-            if(
-                dataObj[i].courseName == '语文' ||
-                dataObj[i].courseName == '数学' ||
-                dataObj[i].courseName == '英语' ||
-                dataObj[i].courseName == '物理' ||
-                dataObj[i].courseName == '化学' ||
-                dataObj[i].courseName == '生物' ||
-                dataObj[i].courseName == '地理' ||
-                dataObj[i].courseName == '历史' ||
-                dataObj[i].courseName == '生物'
-
-            ){
-                courseHtml  += '<tr>'
-                    +'<th class="center">'
-                    +'<label>'
-                    +'<input type="checkbox" dataId="'+ dataObj[i].id +'" courseName="'+ dataObj[i].courseName +'" class="ace" />'
-                    +'<span class="lbl"></span>'
-                    +'</label>'
-                    +'</th>'
-                    +'<td class="center">'+ (i+1) +'</td>'
-                    +'<td class="center">'+ dataObj[i].courseName +'</td>'
-                    +'<td class="center"></td>'
-                    +'<td class="center"></td>'
-                    +'<td class="center"></td>'
-                    +'<td class="center"></td>'
-                    +'</tr>';
-            }
-        }
-        $('#course-tbody').append(courseHtml);
-    },
     // 获取租户下的课程设置信息
-    getCourse:function(){
-        Common.ajaxFun('/course/get/manager/'+ tnId +'.do', 'GET', {}, function (res) {
+    getCourse: function () {
+        Common.ajaxFun('/course/get/manager/' + tnId + '.do', 'GET', {}, function (res) {
             if (res.rtnCode == "0000000") {
                 console.log(res)
+                var dataObj = res.bizData.courses;
+                var courseHtml = '';
+                for (var i = 0; i < dataObj.length; i++) {
+                    courseHtml += '<tr>'
+                        + '<th class="center">'
+                        + '<label>'
+                        + '<input type="checkbox" dataId="' + dataObj[i].id + '" courseName="' + dataObj[i].courseName + '" class="ace" />'
+                        + '<span class="lbl"></span>'
+                        + '</label>'
+                        + '</th>'
+                        + '<td class="center">' + (i + 1) + '</td>'
+                        + '<td class="center">' + dataObj[i].courseName + '</td>'
+                        + '<td class="center"></td>'
+                        + '<td class="center"></td>'
+                        + '<td class="center"></td>'
+                        + '<td class="center"></td>'
+                        + '</tr>';
+                }
+                $('#course-tbody').append(courseHtml);
 
             } else {
                 layer.msg(res.msg);
@@ -75,7 +49,30 @@ CourseManagement.prototype = {
         });
     },
     // 新增课程管理信息
-    addCourse:function(){
+    addCourse: function () {
+        var that = this;
+        Common.ajaxFun('/course/add/manager.do', 'POST', {
+            "clientInfo": {},
+            "style": "",
+            "data": {
+                "courseManage": {
+                    "tnId": "",
+                    "courseBaseId": "",
+                    "gradeId": "",
+                    "courseType": "",
+                    "createTime": ""
+                }
+            }
+        }, function (res) {
+            if (res.rtnCode == "0000000") {
+
+            } else {
+                layer.msg(res.msg);
+            }
+        }, function (res) {
+            layer.msg(res.msg);
+        });
+
 
     },
     queryCourseList: function () {
@@ -83,12 +80,11 @@ CourseManagement.prototype = {
         var courseArr = [];
         courseArr.push('<option value="00">请选择课程名称</option>');
         Common.ajaxFun('/course/get/baseInfo.do', 'GET', {}, function (res) {
-            that.getDefaultInfo(res);
             if (res.rtnCode == "0000000") {
                 var resData = res.bizData.courses;
                 $.each(resData, function (i, v) {
-                    if(i>8){
-                        courseArr.push('<option courseV="' + v['id'] + '" statusV="' + v['status'] + '" value="' + v['courseName'] + '">' + v['courseName'] + '</option>');
+                    if (i > 8) {
+                        courseArr.push('<option courseV="' + v['id'] + '" statusV="' + v['status'] + '" value="' + v['id'] + '">' + v['courseName'] + '</option>');
                     }
                 });
             } else {
@@ -99,17 +95,17 @@ CourseManagement.prototype = {
         }, true);
         return courseArr.join('');
     },
-    addCourseLayer:function(title,id,courseName){
+    addCourseLayer: function (title, id, courseName) {
         var that = this;
         var addCourseContentHtml = [];
         addCourseContentHtml.push('<div class="add-course-box">');
         addCourseContentHtml.push('<div class="course-box">');
         addCourseContentHtml.push('<div class="box-row">');
         addCourseContentHtml.push('<span><i>*</i>课程名称：</span>');
-        if(courseName){
-            addCourseContentHtml.push('<select id="course-name-list" disabled="disabled"><option value="">'+ courseName +'</option></select>');
-        }else{
-            addCourseContentHtml.push('<select id="course-name-list">'+ that.queryCourseList() +'</select>');
+        if (courseName) {
+            addCourseContentHtml.push('<select id="course-name-list" disabled="disabled"><option value="">' + courseName + '</option></select>');
+        } else {
+            addCourseContentHtml.push('<select id="course-name-list">' + that.queryCourseList() + '</select>');
         }
         addCourseContentHtml.push('</div>');
         addCourseContentHtml.push('<div class="box-row">');
@@ -150,14 +146,15 @@ CourseManagement.prototype = {
         });
     },
     // 修改课程管理信息
-    updateCourse:function(){
+    updateCourse: function () {
 
     },
     // 删除课程管理信息
-    delCourse:function(id){
+    delCourse: function (id) {
+        console.log(id)
         var that = this;
         Common.ajaxFun('/course/del/manager/' + id, 'POST', {
-            'courseId':id
+            'courseId': id
         }, function (res) {
             console.log()
             if (res.rtnCode == "0000000") {
@@ -170,7 +167,7 @@ CourseManagement.prototype = {
         });
     },
     //查询年级信息
-    getGrade:function(){
+    getGrade: function () {
         var that = this;
         Common.ajaxFun('/grade/getGrade.do', 'GET', {}, function (res) {
             console.log(res)
@@ -183,24 +180,27 @@ CourseManagement.prototype = {
             layer.msg(res.msg);
         });
     },
-    isSubjectType:function(data){
+    isSubjectType: function (data) {
         var that = this;
         var dataObj = data.bizData;
         // 选择开课年级
-        $('body').on('click','.form-input-checkbox',function(){
+        $('body').on('click', '.form-input-checkbox', function () {
             var id = $(this).attr('data-id');
-            for(var l=0;l<data.bizData.length;l++){
-                if(data.bizData[l].gradeCode==id){
+            var data = '';
+            for (var l = 0; l < data.bizData.length; l++) {
+                if (data.bizData[l].gradeCode == id) {
                     var classType = data.bizData[l].classType;
                     if ($(this).is(':checked')) {
                         $(this).prop('checked', true);
-                        if(classType==3){
-                            $('.box-row-'+classType).removeClass('hides');
+                        console.log(data.bizData[l].gradeCode)
+
+                        if (classType == 3) {
+                            $('.box-row-' + classType).removeClass('hides');
                         }
                     } else {
                         $(this).prop('checked', false);
-                        if(classType==3){
-                            $('.box-row-'+classType).addClass('hides').find('input[type="checkbox"]').prop('checked', false);
+                        if (classType == 3) {
+                            $('.box-row-' + classType).addClass('hides').find('input[type="checkbox"]').prop('checked', false);
                         }
                     }
                 }
@@ -215,11 +215,11 @@ var CourseManagementIns = new CourseManagement();
 $(function () {
 
     // 新增
-    $('#addCourse-btn').on('click',function(){
+    $('#addCourse-btn').on('click', function () {
         CourseManagementIns.addCourseLayer('添加课程');
     });
     // 修改
-    $('#updateCourse-btn').on('click',function(){
+    $('#updateCourse-btn').on('click', function () {
         var id = $('#course-tbody input:checked').attr('dataid'),
             courseName = $('#course-tbody input:checked').attr('courseName'),
             checkboxLen = $('#course-tbody input:checked').length;
@@ -232,10 +232,8 @@ $(function () {
             return false;
         }
 
-        CourseManagementIns.addCourseLayer('修改课程',id,courseName);
+        CourseManagementIns.addCourseLayer('修改课程', id, courseName);
     });
-
-
 
 
     // 删除
@@ -254,7 +252,7 @@ $(function () {
         var courseName = $('#course-tbody input:checked').attr('courseName');
 
 
-        if(
+        if (
             courseName == '语文' ||
             courseName == '数学' ||
             courseName == '英语' ||
@@ -264,28 +262,31 @@ $(function () {
             courseName == '地理' ||
             courseName == '历史' ||
             courseName == '生物'
-        ){
-            layer.tips('不可删除'+ courseName +'课程', $('#course-tbody input:checked'));
+        ) {
+            layer.tips('不可删除' + courseName + '课程', $('#course-tbody input:checked'));
             return false;
         }
-
-
-
-
 
 
         layer.confirm('确定删除?', {
             btn: ['确定', '关闭'] //按钮
         }, function () {
-
-            CourseManagementIns.delCourse(id);
+            //CourseManagementIns.delCourse(id);
         }, function () {
             layer.closeAll();
         });
     });
 
+    // 保存添加
+    $('boay').on('click', '#save-course-btn', function () {
 
+    });
 
+    //
+    $('body').on('change','#course-name-list',function(){
+        var selVal = $(this).val();
+        $('#save-course-btn').attr('courseId',selVal);
+    });
 
 
 });
