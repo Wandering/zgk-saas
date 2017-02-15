f87093e3ff718ea2339fea5dc962974bf805d43c
douzi <douzy75952@hotmail.com>
2017-02-15 14:22:29 +0800

    Course Manage

--- saas-service/src/main/java/cn/thinkjoy/saas/dao/bussiness/ICourseManageDAO.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/dao/bussiness/ICourseManageDAO.java
@@ -2,9 +2,14 @@ package cn.thinkjoy.saas.dao.bussiness;
 
 import cn.thinkjoy.common.dao.IBaseDAO;
 import cn.thinkjoy.saas.domain.bussiness.CourseManage;
+import cn.thinkjoy.saas.domain.bussiness.CourseManageVo;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by douzy on 17/2/13.
  */
 public interface ICourseManageDAO extends IBaseDAO<CourseManage> {
+    List<CourseManageVo> selectCourseManageInfo(Map map);
 }
--- saas-service/src/main/java/cn/thinkjoy/saas/service/ICourseManageService.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/ICourseManageService.java
@@ -4,9 +4,15 @@ import cn.thinkjoy.common.dao.IBaseDAO;
 import cn.thinkjoy.common.domain.BaseDomain;
 import cn.thinkjoy.common.service.IBaseService;
 import cn.thinkjoy.common.service.IPageService;
+import cn.thinkjoy.saas.domain.bussiness.CourseManageVo;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by douzy on 17/2/14.
  */
 public interface ICourseManageService<D extends IBaseDAO<T>, T extends BaseDomain> extends IBaseService<D, T>,IPageService<D, T> {
+
+    List<CourseManageVo> selectCourseManageInfo(Map map);
 }
