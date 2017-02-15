5faa57ec90ed338678c405e1fcfa4ee6653ebcbc
douzi <douzy75952@hotmail.com>
2017-02-15 11:36:18 +0800

    Course Manage

--- saas-service/src/main/java/cn/thinkjoy/saas/service/impl/CourseBaseInfoServiceImpl.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/impl/CourseBaseInfoServiceImpl.java
@@ -2,9 +2,8 @@ package cn.thinkjoy.saas.service.impl;
 
 import cn.thinkjoy.common.dao.IBaseDAO;
 import cn.thinkjoy.common.service.impl.AbstractPageService;
-import cn.thinkjoy.saas.dao.ICountyDAO;
-import cn.thinkjoy.saas.domain.County;
-import cn.thinkjoy.saas.service.ICountyService;
+import cn.thinkjoy.saas.dao.bussiness.ICourseBaseInfoDAO;
+import cn.thinkjoy.saas.domain.bussiness.CourseBaseInfo;
 import cn.thinkjoy.saas.service.ICourseBaseInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,13 +12,13 @@ import org.springframework.stereotype.Service;
  * Created by douzy on 17/2/14.
  */
 @Service("CourseBaseInfoServiceImpl")
-public class CourseBaseInfoServiceImpl extends AbstractPageService<IBaseDAO<County>, County> implements ICourseBaseInfoService<IBaseDAO<County>,County> {
+public class CourseBaseInfoServiceImpl extends AbstractPageService<IBaseDAO<CourseBaseInfo>, CourseBaseInfo> implements ICourseBaseInfoService<IBaseDAO<CourseBaseInfo>,CourseBaseInfo> {
     @Autowired
-    private ICountyDAO countyDAO;
+    private ICourseBaseInfoDAO iCourseBaseInfoDAO;
 
     @Override
-    public IBaseDAO<County> getDao() {
-        return countyDAO;
+    public IBaseDAO<CourseBaseInfo> getDao() {
+        return iCourseBaseInfoDAO;
     }
 
 }
--- saas-service/src/main/java/cn/thinkjoy/saas/service/impl/CourseManageServiceImpl.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/impl/CourseManageServiceImpl.java
@@ -2,9 +2,8 @@ package cn.thinkjoy.saas.service.impl;
 
 import cn.thinkjoy.common.dao.IBaseDAO;
 import cn.thinkjoy.common.service.impl.AbstractPageService;
-import cn.thinkjoy.saas.dao.ICountyDAO;
-import cn.thinkjoy.saas.domain.County;
-import cn.thinkjoy.saas.service.ICountyService;
+import cn.thinkjoy.saas.dao.bussiness.ICourseManageDAO;
+import cn.thinkjoy.saas.domain.bussiness.CourseManage;
 import cn.thinkjoy.saas.service.ICourseManageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,13 +12,13 @@ import org.springframework.stereotype.Service;
  * Created by douzy on 17/2/14.
  */
 @Service("CourseManageServiceImpl")
-public class CourseManageServiceImpl extends AbstractPageService<IBaseDAO<County>, County> implements ICourseManageService<IBaseDAO<County>,County> {
+public class CourseManageServiceImpl extends AbstractPageService<IBaseDAO<CourseManage>, CourseManage> implements ICourseManageService<IBaseDAO<CourseManage>,CourseManage> {
     @Autowired
-    private ICountyDAO countyDAO;
+    private ICourseManageDAO iCourseManageDAO;
 
     @Override
-    public IBaseDAO<County> getDao() {
-        return countyDAO;
+    public IBaseDAO<CourseManage> getDao() {
+        return iCourseManageDAO;
     }
 
 }
