ed3e8ea615975dd5222ef42db38de7e031ec9b6a
douzi <douzy75952@hotmail.com>
2017-02-15 10:58:54 +0800

    Course Manage

--- saas-service/src/main/java/cn/thinkjoy/saas/service/impl/bussiness/EXCourseManageServiceImpl.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/impl/bussiness/EXCourseManageServiceImpl.java
@@ -9,7 +9,7 @@ import javax.annotation.Resource;
 /**
  * Created by douzy on 17/2/14.
  */
-@Service("EXCourseBaseInfoServiceImpl")
+@Service("EXCourseManageServiceImpl")
 public class EXCourseManageServiceImpl implements IEXCourseManageService {
     @Resource
     ICourseManageDAO iCourseManageDAO;
