e75c3bcf505b49f3547902f8eb0709d1b0407d6b
pdeng <pdeng@thinkjoy.cn>
2017-02-19 15:13:45 +0800

    ...

--- saas-service/src/main/java/cn/thinkjoy/saas/service/impl/bussiness/EXScheduleBaseInfoServiceImpl.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/impl/bussiness/EXScheduleBaseInfoServiceImpl.java
@@ -313,7 +313,7 @@ public class EXScheduleBaseInfoServiceImpl implements IEXScheduleBaseInfoService
 //
 //    }
 
-    @Autowired
+    @Override
     public void insertBaseRule(int recordId,int isAttend){
 
         JwTeacher jwTeacher = jwTeacherDAO.fetch(recordId);
