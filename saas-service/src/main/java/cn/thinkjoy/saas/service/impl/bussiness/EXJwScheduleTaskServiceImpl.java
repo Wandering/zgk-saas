cd044f4aec1e83d1d2617ad806e5151e086e511e
杨永平 <ypyang@qtonecloud.cn>
2017-02-20 00:02:18 +0800

    业务:更新成绩

--- saas-service/src/main/java/cn/thinkjoy/saas/service/impl/bussiness/EXJwScheduleTaskServiceImpl.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/impl/bussiness/EXJwScheduleTaskServiceImpl.java
@@ -893,8 +893,8 @@ public class EXJwScheduleTaskServiceImpl implements IEXJwScheduleTaskService {
             List<String> allCourseList = null;
             Map<Integer,String> courses = getCourseByTnIdAndTaskId(tnId,taskId);
             try {
-//                String path = FileOperation.getParamsPath(tnId, taskId);
-                String path = "/Users/yangyongping/Desktop/yqhc/zgk-saas/saas-service/src/main/resources/config/admin_course_0.txt";
+                String path = FileOperation.getParamsPath(tnId, taskId);
+//                String path = "/Users/yangyongping/Desktop/yqhc/zgk-saas/saas-service/src/main/resources/config/admin_course_0.txt";
                 CharSource main = Files.asCharSource(new File(path), Charset.defaultCharset());
                 allCourseList = main.readLines();
             }catch (Exception e){
