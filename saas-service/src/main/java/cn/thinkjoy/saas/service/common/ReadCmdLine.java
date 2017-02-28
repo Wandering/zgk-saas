d68a5eafb39a46d58dc25bc9d21efb1e37a99306
douzi <douzy75952@hotmail.com>
2017-02-28 10:29:08 +0800

    Schedule Task

--- saas-service/src/main/java/cn/thinkjoy/saas/service/common/ReadCmdLine.java
+++ saas-service/src/main/java/cn/thinkjoy/saas/service/common/ReadCmdLine.java
@@ -7,14 +7,22 @@ import java.io.InputStreamReader;
  * Created by douzy on 17/2/21.
  */
 public class ReadCmdLine {
-    public static Integer run(String path) {
+
+    public static final String  SHELL_TIMETABLE_ADJUST="/opt/lib/timetable-adjust.sh %s %s %s %s %s ";
+    public static final String  SHELL_TIMETABLE="/opt/lib/timetable.sh %s %s ";
+
+    public static String timetableShellCmd(String path) {
+        return String.format(SHELL_TIMETABLE, path, path);
+    }
+
+    public static Integer run(String path,String shellCMD) {
         try {
             Runtime rt = Runtime.getRuntime();
-            Process proc = rt.exec("/opt/lib/timetable.sh " + path + " " + path + "");
+            Process proc = rt.exec(shellCMD);
             //读取标准输出流
-            BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             String line;
-            while ((line=bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
             }
             //读取标准错误流
@@ -24,9 +32,9 @@ public class ReadCmdLine {
                 System.out.println(errline);
             }
             //waitFor()判断Process进程是否终止，通过返回值判断是否正常终止。0代表正常终止
-            int c=proc.waitFor();
+            int c = proc.waitFor();
 
-            if(c!=0)
+            if (c != 0)
                 System.out.println("=====================Schedule Task Process进程异常终止=====================");
 
             return c;
